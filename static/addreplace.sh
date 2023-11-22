f44ad25016849dd616ea2aeaa51296beb10f6714
bingis-khan <>
2023-11-23 07:17:52 +0900

    format output better

--- static/addreplace.sh
+++ static/addreplace.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 cd docs
-find pdf -type f | sort | awk '{ print "<li><a href=" $1 ">" $1 "</a></li>" }' > ../links
+find pdf -type f | sort | awk -F'[/.]' '{ print "<li><a href=" $0 ">" $2 "</a></li>" }' > ../links
 cd ..
 
 cat static/index.html | sed '/PDFPATHS/{r links
